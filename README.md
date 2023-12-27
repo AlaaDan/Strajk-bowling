a0bec52eb7065088de38ef3aaae15ab3632b6892
Alaa Aldandachi <audio-net@hotmail.com>
2023-12-27 11:39:39 +0100

    Bug fixes and more test cases

---
+++ README.md
@@ -0,0 +1,15 @@
+#Acceptance criteria
+
+
+1. As a user I should be able to see the inpute fields, Data, time, how many players and how many lanes to be able to make a reservation.
+
+2. As a user I should be able to fill the input fields with my info. 
+
+3. As a user there should be a able to click the strIIIIIike button to make a reservation
+
+4. As a user an error msg should appear indicating that I should fill out all the input fields
+
+5. As a user I should be able to to navigiate between the booking page and the confirmation page
+
+6. When a user make a reservation they should get back the reservation ID as well as the total price for what they have reserved, based on this formula: 120sek/ per person + 100sek per lane.
+
--- src/views/Booking.test.jsx
+++ src/views/Booking.test.jsx
@@ -5,12 +5,27 @@ import Booking from "./Booking";
 import Confirmation from "./Confirmation";
 import { describe, expect } from "vitest";
 import {booking, bookingRes} from "../mocks/handlers";
+import ErrorMessage from "../components/ErrorMessage/ErrorMessage";
 
 const button_text = "strIIIIIike!";
 const x_API_KEY = '738c6b9d-24cf-47c3-b688-f4f4c5747662';
 
 
 describe("Booking", () => {
+    it("should show an error msg if the user fails to input all the reuired fields" , async () => {
+        render(
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Booking />} />
+                    <Route path="/confirmation" element={<Confirmation />} />
+                </Routes>
+            </Router>
+        );
+        fireEvent.click(screen.getByText(button_text ));
+        expect(screen.getByText('Fill out all the fields and make sure that people and shoes is the same number.')).toBeVisible()
+        
+    });
+
     it('should have a button to make a booking', () => {
         render(
             <Router>
@@ -55,7 +70,6 @@ describe("Booking", () => {
     })
 
     it('should return a booking id and the total price when a booking is made', async () => {
-        // using booking object from Booking.test.jsx
         render(
             <Router>
                 <Routes>
@@ -91,23 +105,7 @@ describe("Booking", () => {
         expect(await screen.findByText("See you soon!")).toBeVisible();
         expect(await screen.getByLabelText("Booking number").value).toBe(bookingRes.id);
         // console.log(bookingRes.id)
-        // I don't know why the test fails when I run the test with the following code:
-        // expect(await screen.findByText(`${bookingRes.price} sek`)).toBeVisible();
-        // I get the following error message:
-//         Received element is not visible (element is not in the document):
-//         <p />
-//                  ❯ src/views/Booking.test.jsx:95:68
-//      93|         // console.log(bookingRes.id)
-//      94|         // I don't know why the test fails when I run the test with the following code:
-//      95|         expect(await screen.findByText(`${bookingRes.price} sek`)).toBeVisible();
-//        |                                                                    ^
-//      96|         // I get the following error message:
-//      97|         //
-// I tried everything I could but for some reason the value is not visible, even though the See you soon text as well as
-// the booking number are visible. 
+        expect(await screen.getByText(`${bookingRes.price} sek`)).toBeVisible();
+
     });
-
-
-
-
 });
